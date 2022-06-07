88352e984ce699088928ae2be9ce6ccb9550c36f
kajela <titoyut56@gmail.com>
2022-06-07 01:49:51 -0400

    add the initial files

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,21 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int time = 12;
+
+        if (time < 11) {
+            System.out.println("yes 23 is greater");
+
+        } else if (time >= 12) {
+            System.out.println("it's almost equal to our number");
+
+        } else{
+            System.out.println("time not recognised");
+        }
+        int age = 30;
+        String result = (age >= 30) ? "your a young parent" : " your just a grown up";
+        System.out.println(result);
+
     }
 }
